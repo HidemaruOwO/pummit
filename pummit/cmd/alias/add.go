d001b52a24f02eddc2d1352afeaa11d68650dab4
HidemaruOwO <hidemal@outlook.jp>
2023-03-22 11:30:02 +0900

    :sparkles: WIP (pummit/cmd/alias/add.go, pummit/cmd/alias/delete.go)

--- pummit/cmd/alias/add.go
+++ pummit/cmd/alias/add.go
@@ -1,9 +1,11 @@
-package cmd
+package alias_cmd
+
+import "github.com/HidemaruOwO/nuts/log"
 
 func AliasAddCmd() {
 	aliasAdd()
 }
 
 func aliasAdd() {
-
+	log.Infof("alias add\n")
 }
--- pummit/cmd/alias/delete.go
+++ pummit/cmd/alias/delete.go
@@ -1,9 +1,11 @@
-package cmd
+package alias_cmd
+
+import "github.com/HidemaruOwO/nuts/log"
 
 func AliasDeleteCmd() {
 	aliasDelete()
 }
 
 func aliasDelete() {
-
+	log.Infof("alias delete\n")
 }
