9f321f56619ec64ba7594105e27310ed97f57d65
HidemaruOwO <hidemal@outlook.jp>
2023-03-22 11:17:00 +0900

    :sparkles: リストコマンドの実装#1 (pummit/cmd/alias/list.go)

--- pummit/cmd/alias/list.go
+++ pummit/cmd/alias/list.go
@@ -1,9 +1,24 @@
-package cmd
+package alias_cmd
+
+import (
+	"fmt"
+
+	"github.com/HidemaruOwO/pummit/pummit/lib"
+)
 
 func AliasListCmd() {
-	aliasList()
+	fmt.Printf("%s", aliasList())
 }
 
-func aliasList() {
+func aliasList() string {
+	result := ` 📎 There is aliases
+Alias : Prefix : Emoji
+----------------------
+`
+	for _, value := range lib.GetAliasList() {
+		emojiList := fmt.Sprintf("  %s : %s : %s\n", value[0], value[1], value[2])
+		result = result + emojiList
+	}
 
+	return result
 }
