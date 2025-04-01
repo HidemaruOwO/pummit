ca2ccfb16e48100dac090c025a1241bfc3a755a6
HidemaruOwO <owner@v-sli.me>
2025-04-01 11:15:05 +0800

    ✨ restrict file names length of added files (internal/git/git.go)

--- internal/git/git.go
+++ internal/git/git.go
@@ -1,9 +1,13 @@
 package git
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/HidemaruOwO/pummit/internal/config"
+	"github.com/HidemaruOwO/pummit/pkg/logger"
 )
 
 type CommitMessage struct {
@@ -18,15 +22,59 @@ func IsGitRepository() bool {
 	return err == nil
 }
 
+// TODO これをroot.goで実行するように実装する。
 // コミットを作成
 func Commit(cm CommitMessage) error {
-	message := cm.Emoji + " " + cm.Message
+	log := logger.New()
+
+	// TODO alias.goを直してから実装する、
+	// 参考: app/cmd/root.go:76
+	// cm.EmojiをUseRawEmojiに従ってプレフィックスから絵文字に置き換える
+	if config.CurrentConfig.UseRawEmoji {
+
+	}
+
+	// 変更済みのファイルを取得
+	changed, err := GetChangedFiles()
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+
+	if config.CurrentConfig.UseFilesLength {
+		// コミットメッセージが長くなりすぎないようにするため
+		if config.CurrentConfig.FilesLength < len([]rune(changed)) {
+			changed = fmt.Sprintf("%s...", changed[:config.CurrentConfig.FilesLength])
+		}
+	}
+
+	// message := cm.Emoji + " " + cm.Message + " " + changed
+	message := fmt.Sprintf("%s %s (%s)", cm.Emoji, cm.Message, changed)
 	cmd := exec.Command("git", "commit", "-m", message)
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	return cmd.Run()
+}
+
+func GetChangedFiles() (string, error) {
+	log := logger.New()
+
+	// 変更されたファイルを確認
+	cmd := exec.Command("git", "diff", "--name-only", "--cached")
+	stdout, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	changed := strings.TrimSpace(string(stdout))
+
+	if changed == "" {
+		log.Info("Nothing to changed files.")
+		os.Exit(0)
+	}
+
+	return strings.ReplaceAll(changed, "\n", ", "), nil
 }
 
 // 今のブランチ名の表示
